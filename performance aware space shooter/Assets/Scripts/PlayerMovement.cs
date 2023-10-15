148d188cb831b8e3f7da9e113c3212b38a44c01e
PapperoniHD <gabrielling@hotmail.se>
2023-10-15 18:12:51 +0200

    Added player and shoot function

---
+++ performance aware space shooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotSpeed = 10f;
+    public float maxSpeed = 5f;
+    public float bulletSpeed = 20f;
+
+    private Rigidbody2D rb;
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private Transform bulletSpawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        //rb.AddForce(movement.y * speed * transform.up);
+        rb.velocity = (movement.y * speed * transform.up);
+        rb.rotation += movement.x * rotSpeed * Time.deltaTime;
+
+        if (rb.velocity.y >= maxSpeed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x,maxSpeed);
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPos.position, bulletSpawnPos.rotation);
+        bullet.GetComponent<Rigidbody2D>().velocity = bulletSpeed * transform.up;
+        yield return new WaitForSeconds(1f);
+        Destroy(bullet);
+    }
+}
